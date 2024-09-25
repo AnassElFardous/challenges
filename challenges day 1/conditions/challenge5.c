26d420d465042cdbedf07778964c64064295ee0f
AnassElFardous <fardousanas7@gmail.com>
2024-09-25 21:36:09 +0100

    variable chellenge 5 is fix now

--- challenges day 1/conditions/challenge5.c
+++ challenges day 1/conditions/challenge5.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+
 int main() {
-  int anne,mois,jours,heures,Minutes,Secondes,x;
+    int anne, mois, jours, heures, Minutes, Secondes;
+    int x;
+    char choix;
 
-  printf("entrer lanne :");
-   scanf("%d",&x);
+    printf("Entrer l'annee : ");
+    scanf("%d", &x);
+    getchar();
+    printf("Entrer votre choix (j/m/h/M/s) : ");
+    scanf("%c", &choix);
 
-  jours=anne*365;
-  mois=anne*12;
-  heures=jours*24;
-  Minutes=heures*60;
-  Secondes=Minutes*60;
+    jours = x * 365;
+    mois = x * 12;
+    heures = jours * 24;
+    Minutes = heures * 60;
+    Secondes = Minutes * 60;
 
+    switch (choix) {
+        case 'j':
+            printf("%d jours\n", jours);
+            break;
+        case 'm':
+            printf("%d mois\n", mois);
+            break;
+        case 'h':
+            printf("%d heures\n", heures);
+            break;
+        case 'M':
+            printf("%d minutes\n", Minutes);
+            break;
+        case 's':
+            printf("%d secondes\n", Secondes);
+            break;
+        default:
+            printf("votre choix pas here \n");
+    }
 
-  switch (x) {
-    case"jours":
-    printf("%d jours\n",jours);
-    break;
-  case"mois":
-    printf("%d mois\n",mois);
-    break;
-  case "heures":
-    printf("%d heures\n",heures);
-    break;
-  case "Minutes":
-    printf("%d Minutes\n",Minutes);
-    break;
-  case "Secondes":
-    printf("%d Secondes\n",Secondes);
-    break;
-
-
-
-
+    return 0;
 }
 
-  return 0;
-}
