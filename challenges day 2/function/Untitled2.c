abea862627b845d02ab3d837aff77cce9a3fc4f7
Anass el ferdouss <fardousanas7@gmail.com>
2024-09-27 22:06:49 +0100

    day 2 in function challenge 2 is done

--- challenges day 2/function/Untitled2.c
+++ challenges day 2/function/Untitled2.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int multipliation(int x,int z){
+int mlt;
+mlt=x*z;
+return mlt;
+}
+int main(){
+    int z,x;
+    int mltu;
+printf("entrer numbre x:");
+scanf("%d",&x);
+printf("entrer numbre x:");
+scanf("%d",&z);
+mltu=multipliation(x,z);
+printf("mlt=%d",mltu);
+
+return 0;
+}
 
