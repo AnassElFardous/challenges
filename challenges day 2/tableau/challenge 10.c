ce38fbc171a0aa618ab335b59014fcd6eb828f93
Anass el ferdouss <fardousanas7@gmail.com>
2024-09-29 01:06:25 +0100

    new ex

---
+++ challenges day 2/tableau/challenge 10.c
@@ -0,0 +1,35 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main(){
+int taille,i;
+
+printf("entrer la taille de tableau:");
+scanf("%d",&taille);
+
+int tab[taille];
+
+for(i=0;i<taille;i++){
+    printf("entrer les valeur:");
+    scanf("%d",&tab[i]);
+}
+for (i=0;i<taille;i++)
+{
+    printf("les valeurs %d\n",tab[i]);
+}
+int x;
+printf("chercher:");
+scanf("%d",&x);
+
+int found=0;
+for (i=0;i<taille;i++)
+{
+    if(tab[i]==x){
+       found=1;
+       printf("le nombre trouver%d",x);
+    }
+if(!found){
+        printf("number is not here");
+    }
+}
+    return 0;
+}
