f68e6754d13d4f5478dbc48599c882633d4260f0
unknown <fardousanas7@gmail.com>
2024-09-29 14:03:04 +0200

    i finished challenge 11 in arrays and edit challenge 10

--- challenges day 2/tableau/challenge 11.c
+++ challenges day 2/tableau/challenge 11.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 int main(){
 int taille,i;
@@ -16,19 +17,28 @@ for (i=0;i<taille;i++)
     printf("les valeurs %d\n",tab[i]);
 }
 int x;
+int z;
 printf("chercher:");
 scanf("%d",&x);
-
 int found=0;
 for (i=0;i<taille;i++)
 {
     if(tab[i]==x){
-       found=1;
-       printf("le nombre trouver:%d",x);
+    found=1;
+    printf("number is found:%d\n",x);
+
+    printf("remplacer:");
+    scanf("%d",&z);
+    tab[i]=z;
+    for (i=0;i<taille;i++)
+    {
+        printf("le nombre %d\n",tab[i]);
     }
+
     }
-    if(!found){
-        printf("number is not here");
+}
+if(!found){
+    printf("is not founde");
 }
     return 0;
 }
