122475d0288a88c21dc19b4f267c5d792036782d
Anass el ferdouss <fardousanas7@gmail.com>
2024-10-03 09:11:44 +0100

    projet sas new edit

--- projet-SAS/projet-SAS.c
+++ projet-SAS/projet-SAS.c
@@ -17,21 +17,21 @@ dentaires dent[100];
 
 void ajouter() {
     int taille;
-    printf("Entrez le nombre de clients a ajouter: ");
+    printf("entrez le nombre de clients a ajouter: ");
     scanf("%d", &taille);
 
     for (int i = size; i < size + taille; i++) {
         dent[i].Reference_unique = i + 1;
-        printf("Entrez le nom du client: ");
+        printf("entrez le nom du client: ");
         scanf("%s", dent[i].Nom);
-        printf("Entrez le prenom du client: ");
+        printf("entrez le prenom du client: ");
         scanf("%s", dent[i].Prenom);
-        printf("Entrez l'age du client: ");
+        printf("entrez l'age du client: ");
         scanf("%d", &dent[i].age);
 
         char tele[100];
         do {
-            printf("Entrez le numero de client (format:+212) : ");
+            printf("entrez le numero de client (format:+212) : ");
             scanf("%s", tele);
 
             if (strlen(tele) == 9) {
@@ -43,12 +43,12 @@ void ajouter() {
 
         int choix;
         do {
-            printf("Statut:\n");
-            printf("\t1 - Valide\n");
-            printf("\t2 - Reporte\n");
-            printf("\t3 - Annule\n");
-            printf("\t4 - Traite\n");
-            printf("Votre choix: ");
+            printf("statut:\n");
+            printf("\t1 - valide\n");
+            printf("\t2 - reporte\n");
+            printf("\t3 - annule\n");
+            printf("\t4 - traite\n");
+            printf("votre choix: ");
             scanf("%d", &choix);
 
             switch (choix) {
@@ -65,12 +65,12 @@ void ajouter() {
                 strcpy(dent[i].statut, "traite");
                 break;
             default:
-                printf("Il n'y a que quatre choix!!\n");
+                printf("il n'y a que quatre choix!!\n");
                 break;
             }
         } while (choix < 1 || choix > 4);
 
-        printf("Entrez la date de reservation : ");
+        printf("entrez la date de reservation dd/mm/yy :");
         scanf("%s", dent[i].Date_de_reservation);
     }
 
@@ -81,65 +81,65 @@ void Modifier() {
     int reference, i;
     int trouve = 0;
 
-    printf("Entrez la reference du client a modifier : ");
+    printf("entrez la reference du client a modifier : ");
     scanf("%d", &reference);
 
     for (i = 0; i < size; i++) {
-        if (dent[i].Reference_unique == reference) {
-            printf("Reference trouve pour le client.\n");
+        if (dent[i].reference_unique == reference) {
+            printf("reference trouve pour le client.\n");
             trouve = 1;
             break;
         }
     }
 
     if (!trouve) {
-        printf("Client avec la reference %d non trouve.\n", reference);
+        printf("client avec la reference %d non trouve.\n", reference);
         return;
     }
 
     int choix;
     do {
-        printf("Modifier les informations du client:\n");
-        printf("\t1 - Modifier nom\n");
-        printf("\t2 - Modifier prenom\n");
-        printf("\t3 - Modifier age\n");
-        printf("\t4 - Modifier telephone\n");
-        printf("\t5 - Modifier statut\n");
-        printf("\t6 - Modifier date de reservation\n");
+        printf("modifier les informations du client:\n");
+        printf("\t1 - modifier nom\n");
+        printf("\t2 - modifier prenom\n");
+        printf("\t3 - modifier age\n");
+        printf("\t4 - modifier telephone\n");
+        printf("\t5 - modifier statut\n");
+        printf("\t6 - modifier date de reservation\n");
         printf("\t7 - quitter\n");
-        printf("Votre choix: ");
+        printf("votre choix: ");
         scanf("%d", &choix);
 
         switch (choix) {
         case 1:
-            printf("Entrez le nouveau nom: ");
+            printf("entrez le nouveau nom: ");
             scanf("%s", dent[i].Nom);
             break;
         case 2:
-            printf("Entrez le nouveau prenom: ");
+            printf("entrez le nouveau prenom: ");
             scanf("%s", dent[i].Prenom);
             break;
         case 3:
-            printf("Entrez le nouvel age: ");
+            printf("entrez le nouvel age: ");
             scanf("%d", &dent[i].age);
             break;
         case 4:
-            printf("Entrez le nouveau telephone: ");
+            printf("entrez le nouveau telephone: ");
             scanf("%s", dent[i].Telephone);
             break;
         case 5:
-            printf("Entrez le nouveau statut: ");
+            printf("entrez le nouveau statut: ");
             scanf("%s", dent[i].statut);
             break;
         case 6:
-            printf("Entrez la nouvelle date de reservation: ");
+            printf("entrez la nouvelle date de reservation: ");
             scanf("%s", dent[i].Date_de_reservation);
             break;
         case 7:
-            printf("Quitter le programme.\n");
+            printf("quitter le programme.\n");
             break;
         default:
-            printf("Il n'y a que six choix !!\n");
+            printf("il n'y a que six choix !!\n");
             break;
         }
     } while (choix != 7);
@@ -147,13 +147,13 @@ void Modifier() {
 
 void affichage() {
     for (int i = 0; i < size; i++) {
-        printf("Reference: %d\n", dent[i].Reference_unique);
-        printf("Nom: %s\n", dent[i].Nom);
-        printf("Prenom: %s\n", dent[i].Prenom);
-        printf("Age: %d\n", dent[i].age);
-        printf("Telephone: %s\n", dent[i].Telephone);
-        printf("Statut: %s\n", dent[i].statut);
-        printf("Date de reservation: %s\n", dent[i].Date_de_reservation);
+        printf("reference: %d\n", dent[i].Reference_unique);
+        printf("nom: %s\n", dent[i].Nom);
+        printf("prenom: %s\n", dent[i].Prenom);
+        printf("age: %d\n", dent[i].age);
+        printf("telephone: %s\n", dent[i].Telephone);
+        printf("statut: %s\n", dent[i].statut);
+        printf("date de reservation: %s\n", dent[i].Date_de_reservation);
         printf("---------------------------------\n");
     }
 }
@@ -161,13 +161,13 @@ void supprimer() {
     int j;
     int i;
     char NAME[100];
-    printf("Entrez le nom du client a supprimer: ");
+    printf("entree le nom du client a supprimer: ");
     scanf("%s", NAME);
     int trouve = 0;
 
     for (i = 0; i < size; i++) {
         if (strcmp(dent[i].Nom, NAME) == 0) {
-            printf("Client supprime.\n");
+            printf("client supprime.\n");
             trouve = 1;
 
 
@@ -181,7 +181,7 @@ void supprimer() {
     }
 
     if (!trouve) {
-        printf("Client non trouve.\n");
+        printf("client non trouve.\n");
     }
 }
 void Rechercher() {
@@ -198,7 +198,7 @@ void Rechercher() {
 
         switch (choix) {
         case 1:
-            printf("Entrez le nom a trouver: ");
+            printf("entree le nom a trouver: ");
             scanf("%s", NAME);
             int trouve = 0;
             for (int i = 0; i < size; i++) {
@@ -214,7 +214,7 @@ void Rechercher() {
             break;
 
         case 2:
-            printf("Entrez la reference a trouver: ");
+            printf("entrez la reference a trouver: ");
             scanf("%d", &REFerence);
             trouve = 0;
             for (int i = 0; i < size; i++) {
@@ -225,28 +225,28 @@ void Rechercher() {
                 }
             }
             if (!trouve) {
-                printf("Référence non trouvee.\n");
+                printf("reference non trouvee.\n");
             }
             break;
         case 3:
-            printf("Quitter le programme.\n");
+            printf("quitter le programme.\n");
             break;
         default:
-            printf("Numero non trouve.\n");
+            printf("numero non trouve.\n");
         }
     } while (choix != 3);
 }
 int main() {
     int choix;
     do {
-        printf("Menu :\n");
-        printf("1. Ajouter\n");
-        printf("2. Afficher\n");
-        printf("3. Modifier\n");
+        printf(" menu :\n");
+        printf("1. ajouter\n");
+        printf("2. afficher\n");
+        printf("3. modifier\n");
         printf("4. supprimer\n");
         printf("5. recherch\n");
-        printf("6. Quitter\n");
-        printf("Choix numero du menu : ");
+        printf("6. quitter\n");
+        printf("choix numero du menu : ");
         scanf("%d", &choix);
         printf("--------------------------------\n");
 
@@ -267,10 +267,10 @@ int main() {
             Rechercher();
             break;
         case 6:
-            printf("Quitter le programme.\n");
+            printf("quitter le programme.\n");
             break;
         default:
-            printf("Numero non trouvé.\n");
+            printf("numero non trouve.\n");
         }
     } while (choix != 6);
 
